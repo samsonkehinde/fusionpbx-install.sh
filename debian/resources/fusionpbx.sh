2f53e552169b66a98345efcc2298f1b0ad8d0537
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2021-07-07 14:40:41 +0100

    Update fusionpbx.sh

--- debian/resources/fusionpbx.sh
+++ debian/resources/fusionpbx.sh
@@ -31,5 +31,5 @@ mkdir -p /var/cache/fusionpbx
 chown -R www-data:www-data /var/cache/fusionpbx
 
 #get the source code
-git clone $branch https://github.com/fusionpbx/fusionpbx.git /var/www/fusionpbx
+git clone $branch https://github.com/samsonkehinde/fusionpbx.git /var/www/fusionpbx
 chown -R www-data:www-data /var/www/fusionpbx
